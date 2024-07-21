0c4030bd947106535e80595c70b79b6758c084d4
Rwaa Assi <rwaaassi123@gmail.com>
2024-07-21 17:17:54 +0300

    mapping through songs

--- src/Pages/Songs/Songs.jsx
+++ src/Pages/Songs/Songs.jsx
@@ -1,11 +1,39 @@
-import Song from "../../Components/SongYT"
+import Song from "../../Components/SongYT";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getMusicByType } from "../../redux/thunk/musicsThunk";
 
 const Songs = () => {
+  const dispatch = useDispatch();
+  const musics = useSelector((state) => state.musics.data);
+
+  useEffect(() => {
+    dispatch(getMusicByType("numbers"));
+    dispatch(getMusicByType("alphabet"));
+    dispatch(getMusicByType("general"));
+  }, [dispatch]);
+
+  const numbersSongs = musics?.filter((song) => song.type === "numbers") || [];
+  const alphabetSongs =
+    musics?.filter((song) => song.type === "alphabet") || [];
+  const generalSongs = musics?.filter((song) => song.type === "general") || [];
+
   return (
     <div>
-      <h1 className="">Learn with us!</h1>
-      <Song videoId="eBVqcTEC3zQ" />
+      <h1>Learn with us!</h1>
+      <h2>Numbers Songs</h2>
+      {numbersSongs.map((song) => (
+        <Song key={song.id} videoId={song.videoId} />
+      ))}
+      <h2>Alphabet Songs</h2>
+      {alphabetSongs.map((song) => (
+        <Song key={song.id} videoId={song.videoId} />
+      ))}
+      <h2>General Songs</h2>
+      {generalSongs.map((song) => (
+        <Song key={song.id} videoId={song.videoId} />
+      ))}
     </div>
   );
-}
-export default Songs
+};
+export default Songs;
--- src/redux/thunk/musicsThunk.js
+++ src/redux/thunk/musicsThunk.js
@@ -2,13 +2,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../axiosConfig";
 
 export const getMusicByType = createAsyncThunk(
-  "musics/fetchMusicByType",
+  "music/fetchMusicByType",
   async (type, { rejectWithValue }) => {
     try {
-      const response = await axios.get(`musics?type=${type}`);
+      const response = await axios.get(`music?type=${type}`);
+      console.log(response.data);
       return response.data.data;
     } catch (err) {
-      console.error(`Error fetching musics of type ${type}:`, err);
+      console.error(`Error fetching music of type ${type}:`, err);
       return rejectWithValue(err.response?.data);
     }
   }
