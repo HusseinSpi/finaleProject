1f3883296edf88854f4a6e77aa39e6643745e4c4
Rwaa Assi <rwaaassi123@gmail.com>
2024-07-21 16:09:28 +0300

    getting songs from youtube

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NotFound from "./Pages/NotFound/NotFound";
 import Home from "./Pages/Home/Home";
+import Songs from "./Pages/Songs/Songs";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "songs",
+    element: <Songs/>
   },
   {
     path: "*",
