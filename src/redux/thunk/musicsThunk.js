c82cdf531eff60f3aba895ce62011caf22e6cfcb
HusseinSpi <Huseeink852@gmail.com>
2024-07-21 17:12:00 +0300

    redux new

--- src/redux/thunk/musicsThunk.js
+++ src/redux/thunk/musicsThunk.js
@@ -1,14 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../axiosConfig";
 
-export const getMusicByType = createAsyncThunk(
+export const getAllMusic = createAsyncThunk(
   "musics/fetchMusicByType",
-  async (type, { rejectWithValue }) => {
+  async ({ rejectWithValue }) => {
     try {
-      const response = await axios.get(`musics?type=${type}`);
+      const response = await axios.get(`musics`);
       return response.data.data;
     } catch (err) {
-      console.error(`Error fetching musics of type ${type}:`, err);
+      console.error(`Error fetching musics:`, err);
       return rejectWithValue(err.response?.data);
     }
   }
