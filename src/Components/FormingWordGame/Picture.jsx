0a4fcc3b92e0ec949297ff4ebd4b872b90f1b434
TanteDidi <danielarmoni@yahoo.com>
2024-07-23 10:17:47 +0300

    changes alt description

--- src/Components/FormingWordGame/Picture.jsx
+++ src/Components/FormingWordGame/Picture.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Picture = ({ url }) => {
   return (
     <div className="picture">
-      <img src={url} alt="Form the word" />
+      <img src={url} alt="Animal pic that is loading..." />
     </div>
   )
 }
