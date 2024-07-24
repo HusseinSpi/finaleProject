8e073ca6832f155d49a0ff3bb5d4ccb88bafadd4
HusseinSpi <Huseeink852@gmail.com>
2024-07-24 23:32:32 +0300

    50

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -1,28 +1,73 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import { FaArrowUp, FaRobot } from "react-icons/fa";
 import { IoChatboxEllipses, IoPersonSharp } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
 import { getReplyFromAiChat } from "../../redux/thunk/chatWithAiThunk";
+import { getCurrentUser } from "../../redux/thunk/currentUserThunks";
+import { io } from "socket.io-client";
 
 const Chat = () => {
   const dispatch = useDispatch();
   const chatWithAi = useSelector((state) => state.chatWithAi.data);
+  const currentUser = useSelector(
+    (state) => state.currentUser.data?.data?.user
+  );
   const [isOpen, setIsOpen] = useState(false);
   const [messageType, setMessageType] = useState("Ai");
-  const [aiMessages, setAiMessages] = useState([]);
-  const [personMessages, setPersonMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+  let socket;
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (messageType === "Specialist" && currentUser) {
+      socket = io("http://localhost:3000", {
+        withCredentials: true,
+        extraHeaders: {
+          "my-custom-header": "abcd",
+        },
+      });
+
+      socket.on("connect", () => {
+        console.log("Connected to server");
+        socket.emit("joinRoom", currentUser._id);
+      });
+
+      socket.on("loadMessages", (loadedMessages) => {
+        setMessages(
+          loadedMessages.map((msg) => ({ ...msg, type: "Specialist" }))
+        );
+      });
+
+      socket.on("receiveMessage", (message) => {
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { ...message, type: "Specialist" },
+        ]);
+      });
+
+      return () => {
+        socket.disconnect();
+      };
+    }
+  }, [messageType, currentUser]);
 
   useEffect(() => {
     if (chatWithAi && messageType === "Ai") {
       if (
-        aiMessages.length !== 0 &&
-        aiMessages[aiMessages.length - 1] !== chatWithAi
+        messages.length !== 0 &&
+        messages[messages.length - 1].sender === "user"
       ) {
-        setAiMessages((prevMessages) => [...prevMessages, chatWithAi]);
-        console.log(aiMessages);
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { text: chatWithAi, type: "Ai", sender: "bot" },
+        ]);
       }
     }
   }, [chatWithAi]);
@@ -45,13 +90,29 @@ const Chat = () => {
 
   const handleSendMessage = () => {
     if (inputValue.trim() !== "") {
+      const newMessage = {
+        text: inputValue,
+        type: messageType,
+        sender: "user",
+        socketMessage: {
+          senderId: currentUser._id,
+          // recipientId,
+          message: inputValue,
+        },
+      };
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
       if (messageType === "Ai") {
-        setAiMessages((prevMessages) => [...prevMessages, inputValue]);
         sendToAi(inputValue);
-      } else {
-        setPersonMessages((prevMessages) => [...prevMessages, inputValue]);
+      } else if (socket) {
+        socket.emit("sendMessage", newMessage.socketMessage);
       }
       setInputValue("");
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
     }
   };
 
@@ -74,27 +135,28 @@ const Chat = () => {
               </div>
             </div>
             <div className="bg-gray-100 p-2 rounded mb-4 h-chat flex-col content-end overflow-y-auto">
-              {(messageType === "Ai" ? aiMessages : personMessages).map(
-                (msg, index) => (
+              {messages
+                .filter((msg) => msg.type === messageType)
+                .map((msg, index) => (
                   <div
                     key={index}
                     className={`mb-2 p-2 rounded ${
-                      index % 2 === 0
-                        ? "bg-green-200 text-right"
-                        : "bg-stone-300 text-left"
+                      msg.sender === "user"
+                        ? "bg-green-200 text-right self-end"
+                        : "bg-stone-300 text-left self-start"
                     }`}
                   >
-                    {msg}
+                    {msg.text}
                   </div>
-                )
-              )}
-
+                ))}
               <div className="flex items-center border-2 border-green-500 rounded-full p-1">
                 <input
                   type="text"
                   placeholder="Message..."
                   value={inputValue}
                   onChange={handleInputChange}
+                  onKeyPress={handleKeyPress}
+                  ref={inputRef}
                   className="flex-1 border-none outline-none px-4 py-2 rounded-full mr-3"
                 />
                 <div className="flex items-center space-x-2">
@@ -119,9 +181,9 @@ const Chat = () => {
               </button>
               <button
                 className={`text-black ${
-                  messageType === "Person" ? "text-green-500" : ""
+                  messageType === "Specialist" ? "text-green-500" : ""
                 }`}
-                onClick={() => handleMessageTypeChange("Person")}
+                onClick={() => handleMessageTypeChange("Specialist")}
               >
                 <IoPersonSharp className="m-auto text-2xl" />
                 Virtual Specialist
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import videoBg from "./pictures/videoBg.mp4";
-import tunnel from "./pictures/tunnel.png";
 import game from "./pictures/game.png";
 import stories from "./pictures/stories.jpeg";
 import song from "./pictures/song.jpeg";
 import parents from "./pictures/parents.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <div className="h-screen w-full relative">
+      <div className="relative h-screen w-full">
         <video
           className="absolute top-0 left-0 w-full h-full object-cover"
           src={videoBg}
@@ -18,21 +18,28 @@ const Home = () => {
         />
         <div className="relative z-10 flex items-center justify-center h-full w-full"></div>
       </div>
-      <div className="bg-yellow-900 relative">
-        <img className="m-auto p-16" src={tunnel} alt="tunnel" />
-        <div className="absolute top-0 left-0 w-full h-full flex flex-wrap justify-center items-center">
-          <ul className="flex flex-wrap justify-center items-center">
+      <div className="bg-yellow-900 py-10">
+        <div className="flex flex-wrap justify-center items-center gap-20">
+          <ul className="flex flex-wrap justify-center items-center gap-20">
             <li className="circle">
-              <img className="circle-img" src={game} alt="game" />
+              <Link to="/games">
+                <img className="circle-img" src={game} alt="game" />
+              </Link>
             </li>
             <li className="circle">
-              <img className="circle-img" src={stories} alt="stories" />
+              <Link to="/stories">
+                <img className="circle-img" src={stories} alt="stories" />
+              </Link>
             </li>
             <li className="circle">
-              <img className="circle-img" src={song} alt="song" />
+              <Link to="/songs">
+                <img className="circle-img" src={song} alt="song" />
+              </Link>
             </li>
             <li className="circle">
-              <img className="circle-img" src={parents} alt="parents" />
+              <Link to="/parents">
+                <img className="circle-img" src={parents} alt="parents" />
+              </Link>
             </li>
           </ul>
         </div>
