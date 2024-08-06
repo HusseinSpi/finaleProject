c1271c540aeff85acdf65a89cebed3cd865f9e57
TanteDidi <danielarmoni@yahoo.com>
2024-08-07 02:20:04 +0300

    adding to the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,23 @@
-import underwater from "./pictures/underwater.mp4";
+import underwater from './pictures/underwater.mp4'
 // import underwater from "../../../public/underwater1.mp4";
-import starfish from "./pictures/starfish.png";
-import background from "../../../public/bbg.jpg";
-import { GiMeal, GiCoral, GiSeaStar } from "react-icons/gi";
-import { MdOutlineMenuBook } from "react-icons/md";
-import { HiOutlineMusicNote } from "react-icons/hi";
-import { RiParentLine } from "react-icons/ri";
-import { LuGamepad2 } from "react-icons/lu";
-import { Link } from "react-router-dom";
-import "../../index.css";
-import bubble from "../../../public/bubble.png";
-import { useTranslation } from "react-i18next";
-import seashell from "../../../public/seashell2.png";
-import coral from "../../../public/coral.png";
-import seahorse from "../../../public/seahorse.png";
-import crab from "../../../public/crab.png";
+import starfish from './pictures/starfish.png'
+import background from '../../../public/bbg.jpg'
+import { GiMeal, GiCoral, GiSeaStar } from 'react-icons/gi'
+import { MdOutlineMenuBook } from 'react-icons/md'
+import { HiOutlineMusicNote } from 'react-icons/hi'
+import { RiParentLine } from 'react-icons/ri'
+import { LuGamepad2 } from 'react-icons/lu'
+import { Link } from 'react-router-dom'
+import '../../index.css'
+import bubble from '../../../public/bubble.png'
+import { useTranslation } from 'react-i18next'
+import seashell from '../../../public/seashell2.png'
+import coral from '../../../public/coral.png'
+import seahorse from '../../../public/seahorse.png'
+import crab from '../../../public/crab.png'
 
 const Home = () => {
-  const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation()
   return (
     <div>
       <div className="relative h-screen w-full overflow-hidden">
@@ -30,26 +30,28 @@ const Home = () => {
         />
         <div className="absolute top-[7rem] flex flex-col  items-center left-0 w-full p-4 text-xl font-extrabold leading-8 tracking-wider text-center">
           <h1 className=" text-3xl md:text-5xl font-bold flex mb-4 text-white">
-            {i18n.language === "en" ? (
+            {i18n.language === 'en' ? (
               <>
-                <GiCoral /> {t("Welcome")} KiddoFun <GiCoral />
+                <GiCoral /> {t('Welcome')} KiddoFun <GiCoral />
               </>
             ) : (
               <>
-                <GiCoral /> KiddoFun {t("Welcome")}
+                <GiCoral /> KiddoFun {t('Welcome')}
                 <GiCoral />
               </>
             )}
           </h1>
-          <p className="text-white w-2/3 text-base sm:text-lg md:text-xl lg:text-xl font-bold leading-tight mb-4">
-            A world where learning meets adventure! Dive into a world of
-            interactive games, enchanting stories, and delightful songs designed
-            to spark curiosity and imagination in every child.
+          <p className="text-white w-2/3 text-base sm:text-lg md:text-xl lg:text-xl   font-bold leading-tight mb-4">
+            A world where learning meets adventure!
+            <br /> Dive with us into a world of interactive Games,
+            Enchanting-Stories, and Delightful Songs <br />
+            Designed to spark curiosity and imagination in every child.
           </p>
           <p className="text-white w-2/3 text-base sm:text-lg md:text-xl lg:text-xl font-bold leading-tight">
             Discover a world of resources designed for both children and
-            parents, offering a blend of educational videos, engaging articles,
-            and enriching books to support your family’s growth and learning
+            parents, <br />
+            Offering a blend of Educational Videos, Engaging Articles, and
+            Enriching Books to support your family’s growth and learning
             journey.
           </p>
         </div>
@@ -57,12 +59,12 @@ const Home = () => {
 
       <div className="relative flex flex-col items-center justify-center h-[12rem] p-1 custom-radial">
         <div className="flex flex-col items-center justify-center h-full w-full bg-white font-bold">
-          <p className="text-md md:text-xl lg:text-xl text-center">
+          <p className="text-md md:text-xl lg:text-xl text-center text-blue-900">
             “A child’s mind is like a sponge, absorbing everything around it.
             Through play and imagination, they transform the ordinary into the
             extraordinary.”
           </p>
-          <span>— Albert Einstein</span>
+          <span className="text-blue-900">— Albert Einstein</span>
         </div>
       </div>
 
@@ -70,15 +72,15 @@ const Home = () => {
         className="w-screen h-screen p-5 flex flex-col items-center justify-evenly relative "
         style={{
           background: `url(${background})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          width: "100%",
-          height: "100vh",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          width: '100%',
+          height: '100vh',
         }}
       >
         <div className="text-center">
           <h1 className="text-blue-900 text-4xl flex font-bold ">
-            <GiSeaStar /> {t("KidsSection")} <GiSeaStar />
+            <GiSeaStar /> {t('KidsSection')} <GiSeaStar />
           </h1>
         </div>
         <div className="flex flex-wrap justify-center gap-8 -mt-6 relative">
@@ -87,13 +89,13 @@ const Home = () => {
               className="relative w-[12rem] h-[12rem] rounded-full flex flex-col gap-2 items-center justify-center text-blue-900 text-xl font-semibold"
               style={{
                 backgroundImage: `url(${bubble})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                padding: "2rem",
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                padding: '2rem',
               }}
             >
-              {t("Games")} <LuGamepad2 size={40} color="#2E1065" />
+              {t('Games')} <LuGamepad2 size={40} color="#2E1065" />
               <img
                 src={starfish}
                 alt="Starfish"
@@ -106,13 +108,13 @@ const Home = () => {
               className="relative w-[12rem] h-[12rem] rounded-full bg-[#B5A5D0] flex flex-col gap-2 items-center justify-center text-blue-900 text-xl font-semibold"
               style={{
                 backgroundImage: `url(${bubble})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                padding: "2rem",
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                padding: '2rem',
               }}
             >
-              {t("Stories")} <MdOutlineMenuBook size={40} color="#2E1065" />
+              {t('Stories')} <MdOutlineMenuBook size={40} color="#2E1065" />
               <img
                 src={seashell}
                 alt="Seashell"
@@ -125,13 +127,13 @@ const Home = () => {
               className="relative w-[12rem] h-[12rem] rounded-full bg-[#B5A5D0] flex flex-col gap-2 items-center justify-center text-blue-900 text-xl font-semibold"
               style={{
                 backgroundImage: `url(${bubble})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                padding: "2rem",
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                padding: '2rem',
               }}
             >
-              {t("Songs")} <HiOutlineMusicNote size={40} color="#2E1065" />
+              {t('Songs')} <HiOutlineMusicNote size={40} color="#2E1065" />
               <img
                 src={coral}
                 alt="Coral"
@@ -143,7 +145,7 @@ const Home = () => {
 
         <div className="text-center ">
           <h1 className="text-blue-900 text-4xl flex font-bold">
-            <GiSeaStar /> {t("ParentsSection")} <GiSeaStar />
+            <GiSeaStar /> {t('ParentsSection')} <GiSeaStar />
           </h1>
         </div>
         <div className="flex flex-wrap justify-center gap-8 relative -mt-6">
@@ -152,13 +154,13 @@ const Home = () => {
               className="relative w-[12rem] h-[12rem] rounded-full bg-[#B5A5D0] flex flex-col gap-2 items-center justify-center text-blue-900 text-xl font-semibold text-center"
               style={{
                 backgroundImage: `url(${bubble})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                padding: "2rem",
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                padding: '2rem',
               }}
             >
-              {t("MealRecipes")} <GiMeal size={40} color="#2E1065" />
+              {t('MealRecipes')} <GiMeal size={40} color="#2E1065" />
               <img
                 src={seahorse}
                 alt="Seahorse"
@@ -171,13 +173,13 @@ const Home = () => {
               className="relative w-[12rem] h-[12rem] text-center rounded-full bg-[#B5A5D0] flex flex-col gap-2 items-center justify-center text-blue-900 text-xl font-semibold"
               style={{
                 backgroundImage: `url(${bubble})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                padding: "2rem",
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                padding: '2rem',
               }}
             >
-              {t("ParentingTips")} <RiParentLine size={40} color="#2E1065" />
+              {t('ParentingTips')} <RiParentLine size={40} color="#2E1065" />
               <img
                 src={crab}
                 alt="Crab"
@@ -188,7 +190,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,4 +1,79 @@
+import './NotFound.css'
+
 const NotFound = () => {
-  return <div>NotFound</div>;
-};
-export default NotFound;
+  return (
+    <div>
+      <>
+        <div className="NotFound-main_wrapper">
+          <div className="NotFound-main">
+            <div className="NotFound-antenna">
+              <div className="NotFound-antenna_shadow" />
+              <div className="NotFound-a1" />
+              <div className="NotFound-a1d" />
+              <div className="NotFound-a2" />
+              <div className="NotFound-a2d" />
+              <div className="NotFound-a_base" />
+            </div>
+            <div className="NotFound-tv">
+              <div className="NotFound-cruve">
+                <svg
+                  xmlSpace="preserve"
+                  viewBox="0 0 189.929 189.929"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                  className="NotFound-curve_svg"
+                >
+                  <path
+                    d="M70.343,70.343c-30.554,30.553-44.806,72.7-39.102,115.635l-29.738,3.951C-5.442,137.659,11.917,86.34,49.129,49.13
+  C86.34,11.918,137.664-5.445,189.928,1.502l-3.95,29.738C143.041,25.54,100.895,39.789,70.343,70.343z"
+                  />
+                </svg>
+              </div>
+              <div className="NotFound-display_div">
+                <div className="NotFound-screen_out">
+                  <div className="NotFound-screen_out1">
+                    <div className="NotFound-screen">
+                      <span className="NotFound-notfound_text"> NOT FOUND</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="NotFound-lines">
+                <div className="NotFound-line1" />
+                <div className="NotFound-line2" />
+                <div className="NotFound-line3" />
+              </div>
+              <div className="NotFound-buttons_div">
+                <div className="NotFound-b1">
+                  <div />
+                </div>
+                <div className="NotFound-b2" />
+                <div className="NotFound-speakers">
+                  <div className="NotFound-g1">
+                    <div className="NotFound-g11" />
+                    <div className="NotFound-g12" />
+                    <div className="NotFound-g13" />
+                  </div>
+                  <div className="NotFound-g" />
+                  <div className="NotFound-g" />
+                </div>
+              </div>
+            </div>
+            <div className="NotFound-bottom">
+              <div className="NotFound-base1" />
+              <div className="NotFound-base2" />
+              <div className="NotFound-base3" />
+            </div>
+          </div>
+          <div className="NotFound-text_404">
+            <div className="NotFound-text_4041">4</div>
+            <div className="NotFound-text_4042">0</div>
+            <div className="NotFound-text_4043">4</div>
+          </div>
+        </div>
+      </>
+    </div>
+  )
+}
+export default NotFound
